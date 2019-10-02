f8640581349b0e9b8b42cca6272e779df9880894
Preetee <Preetee@Dhirajs-MacBook-Pro.local>
2019-10-02 19:07:15 +0400

    implemented a cart.utils with a addItemToCart function to add multiple items to cart, updated shopdata

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,19 @@
+
+/*
+verify if new item already exist in cart
+Increment quantity if already exists
+else quantity is set to 1 if new item
+*/
+export const addItemToCart = (cartItems,cartItemToAdd)=>{
+  const existingCartItem = cartItems.find(
+    cartItem=> cartItem.id===cartItemToAdd.id
+  )
+
+  if(existingCartItem){
+   return cartItems.map(cartItem=>
+      cartItem.id=== cartItemToAdd.id ?
+        {...cartItems,quantity:cartItem.quantity+1} : cartItem
+      )
+  }
+  return [...cartItems,{...cartItemToAdd,quantity:1}]
+}
