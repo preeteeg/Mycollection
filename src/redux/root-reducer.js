a0faa8f7816ff9cda6aaf27cd62e865e7aadbf2a
Preetee <Preetee@Dhirajs-MacBook-Pro.local>
2019-10-04 14:59:24 +0400

    implemented a local storage using redux-persist for cart

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,11 +1,27 @@
 //all the reducers will be in the root reducer
 //actual base reducer object of all states combined in our application
 
-
 import {combineReducers} from 'redux'
 import userReducer from './user/user.reducer'
 import cartReducer from './cart/cart.reducer'
-export default combineReducers({
+import {persistReducer} from 'redux-persist'
+//local storage
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig={
+    key:'root',
+    storage,
+    whitelist:['cart']//array to store the reducer in local storage
+}
+
+// export default combineReducers({
+//     user:userReducer,
+//     cart:cartReducer
+// })
+
+const rootReducer= combineReducers({
     user:userReducer,
     cart:cartReducer
 })
+
+export default persistReducer(persistConfig,rootReducer)
